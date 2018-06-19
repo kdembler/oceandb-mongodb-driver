4bb30c96f3383b0495a988228b1dfb15252af12f
eruizgar91 <enrique@oceanprotocol.com>
2018-06-19 17:16:53 +0200

    Update oceandb driver interface version

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ with open('README.rst') as readme_file:
 with open('HISTORY.rst') as history_file:
     history = history_file.read()
 
-requirements = ['oceandb-driver-interface==0.1.1', ]
+requirements = ['oceandb-driver-interface==0.0.1', ]
 
 setup_requirements = ['pytest-runner', ]
 
