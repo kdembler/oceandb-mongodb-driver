48d8fd725b3721396491d9f642ea611584bc3e8c
eruizgar91 <enrique@oceanprotocol.com>
2019-02-13 16:01:22 +0100

    Create own function per query

--- oceandb_mongodb_driver/utils.py
+++ oceandb_mongodb_driver/utils.py
@@ -15,14 +15,7 @@ def query_parser(query):
     query_result = {}
     for key in query.items():
         if 'price' in key:
-            if len(query['price']) > 2:
-                logger.info('You are sending more values than needed.')
-            elif len(query['price']) == 0:
-                logger.info('You are not sending any value.')
-            elif len(query['price']) == 1:
-                query_result[index.price] = {GT: 0, LT: query['price'][0]}
-            else:
-                query_result[index.price] = {GT: query['price'][0], LT: query['price'][1]}
+            create_price_query(query, query_result)
         elif 'license' in key:
             create_query(query['license'], index.license, query_result, OR)
         elif 'categories' in key:
@@ -32,19 +25,7 @@ def query_parser(query):
         elif 'updateFrequency' in key:
             create_query(query['updateFrequency'], index.updated_frequency, query_result, OR)
         elif 'created' in key:
-            now = datetime.now() - timedelta(weeks=1000)
-            for values in query['created']:
-                if values == 'today':
-                    now = datetime.now() - timedelta(days=1)
-                elif values == 'lastWeek':
-                    now = datetime.now() - timedelta(days=7)
-                elif values == 'lastMonth':
-                    now = datetime.now() - timedelta(days=30)
-                elif values == 'lastYear':
-                    now = datetime.now() - timedelta(days=365)
-                else:
-                    logger.info('The key %s is not supported in the created query' % values)
-            query_result['created'] = {"$gt": now}
+            create_created_query(query, query_result)
         elif 'sample' in key:
             query_result[index.sample] = 'sample'
         else:
@@ -63,3 +44,32 @@ def create_query(value, index, query, operator):
         else:
             query[operator] += [{index: value[i]}]
     return query
+
+
+def create_price_query(query, query_result):
+    if len(query['price']) > 2:
+        logger.info('You are sending more values than needed.')
+    elif len(query['price']) == 0:
+        logger.info('You are not sending any value.')
+    elif len(query['price']) == 1:
+        query_result[index.price] = {GT: 0, LT: query['price'][0]}
+    else:
+        query_result[index.price] = {GT: query['price'][0], LT: query['price'][1]}
+    return query_result
+
+
+def create_created_query(query, query_result):
+    now = datetime.now() - timedelta(weeks=1000)
+    for values in query['created']:
+        if values == 'today':
+            now = datetime.now() - timedelta(days=1)
+        elif values == 'lastWeek':
+            now = datetime.now() - timedelta(days=7)
+        elif values == 'lastMonth':
+            now = datetime.now() - timedelta(days=30)
+        elif values == 'lastYear':
+            now = datetime.now() - timedelta(days=365)
+        else:
+            logger.info('The key %s is not supported in the created query' % values)
+    query_result['created'] = {GT: now}
+    return query_result
